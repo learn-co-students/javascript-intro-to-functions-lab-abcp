17b82a9d90f8a76cad938d5f2740ff4c7a5e4c6b
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-09 11:52:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  string=string.toUpperCase()
+  console.log(string)
+}
+
+function logWhisper(string){
+  string=string.toLowerCase()
+  console.log(string)
+}
+
+function sayHiToGrandma(string){
+  if(string===lowercase)
+  {
+    return "I can't hear you!"
+  }
+}
