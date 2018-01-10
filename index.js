7c70fe6f92523823cc525596418665f6fdcc49c6
Valerie Wood <valeriewd1@gmail.com>
2018-01-10 18:17:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
+
 function shout(string) {
-  return string.toUpperCase();
+  return string.toUpperCase()
 }
 
 function whisper(string) {
-return string.toLowerCase();
+  return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase());
+  console.log(string.toUpperCase())
 }
 
-function logWhisper(string) { 
-  return ();
+function logWhisper(string) {
+  console.log(string.toLowerCase())
 }
-console.log(string.toLowerCase());
 
- function sayHiToGrandma(string) {
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
-  console.log(string.toLowerCase("I can't hear you!"));
 
-function sayHiToGrandma(string) {
-  return string.toUpperCase();
-}
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
 
-function sayHiToGrandma(string) {
-  return string.toUpperCase();
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+
+  return "Are you eating enough?"
 }
