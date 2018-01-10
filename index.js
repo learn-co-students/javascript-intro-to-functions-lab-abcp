2d5206d8b5ef9be7fed609a386ad300b9a65f5c7
Valerie Wood <valeriewd1@gmail.com>
2018-01-10 04:25:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) { 
+  return ();
+}
+console.log(string.toLowerCase());
+
+ function sayHiToGrandma(string) {
+  }
+  console.log(string.toLowerCase("I can't hear you!"));
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase();
+}
+
+function sayHiToGrandma(string) {
+  return string.toUpperCase();
+}
