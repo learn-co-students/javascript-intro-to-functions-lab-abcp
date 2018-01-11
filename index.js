bbbc6372d7dbc6d1c40b571662c1e8b682ef75fe
Nicole Rhoades <nrhoa001@gmail.com>
2018-01-11 21:16:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 function shout(string) {
  return string.toUpperCase()
  }
+ 
 function whisper(string) {
  return string.toLowerCase()
  }
+ 
 function logShout(string) { 
-  console.log(string.toUpperCase()
-)}
+  console.log(string.toUpperCase())
+}
+
 function logWhisper(string) {
-  console.log(string.toLowerCase()
- )}
+  console.log(string.toLowerCase())
+ }
+ 
   function sayHiToGrandma(string) {
-  return (string.toUpperCaseCase ("HELLO!")
-  )}
-  function sayHiToGrandma(string) {
-    return (string.toLowerCase ("I can't hear you !")
- )}
- function sayHiToGrandma(string) {
-   return (string.toUpperCase("YES INDEED!")
- )}
+  if (string.toLowerCase()=== string) {
+  return"I can't hear you!"
+  }
+  
+  if(string.toUpperCase()=== string) {
+    return "YES INDEED!"
+ }
+ 
+ if (string==="I love you, Grandma."){
+   return "I love you, too."
+ }
+ 
+ return "I love you, too."
+  }
