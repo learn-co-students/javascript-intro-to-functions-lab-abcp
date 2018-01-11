26fc998f1b153d2d31571d1bc61874ee6b3af067
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-11 18:45:38 +0000

    Done.

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase() === string) {
+    return("I can't hear you!");
+  }
+  else if (string.toUpperCase() === string) {
+    return ("Yes, indeed!")
+  }
+  
+}
