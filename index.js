12712577448b49047f1aaa2981bdc7b618d4edef
Melinda Burke <melccb@gmail.com>
2018-01-12 05:46:05 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout (string) {
+  return string.toUpperCase()
+  
+}
+
+function whisper (string) {
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma (string)  {
+  console.log('I can\'t hear you');
+  var lowercase = 'I can\'t hear you';
+  lowercase.toLowerCase() === lowercase
+  
+}
