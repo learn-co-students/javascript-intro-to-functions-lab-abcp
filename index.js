07f6e09274be13c1f18f9a910d516b653d7d3b98
Nicolas Corbalan Calvo <outreef@hotmail.com>
2018-01-12 16:33:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout('hello') {
+  console.log('HELLO')
+}
+
+function sayHiToGrandma() {
+  console.log("I love you, Grandma.")
+}
