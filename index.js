f3a5a341332ac44570f7ea24ad042c4fc5426018
Jay Bednar <ironviralmedia@gmail.com>
2018-01-13 18:02:53 +0000

    Done.

--- index.js
+++ index.js
@@ -6,23 +6,22 @@ function whisper(string) {
   return string.toLowerCase();
 }
 
-
-function logShout(string) {
+function logShout(string) { 
   console.log(string.toUpperCase());
 }
-logShout("hello");
 
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
-logWhisper('HELLO');
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string) {
+  if (string === string.toLowerCase()) {
     return "I can\'t hear you!";
-  } else if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
+  } else {
+    return "Say again.";
   }
-}
+} 
