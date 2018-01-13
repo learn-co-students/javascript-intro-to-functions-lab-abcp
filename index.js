26b897c5ec4aad08c89f7ccc3dd054784a02b988
Lori Kulikowski <soupfairy99@gmail.com>
2018-01-13 11:43:50 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,8 @@
+function shout(string) {
+return string.toUpperCase()
+}
+function whisper(string2) {
+return string2.toLowerCase()
+}
+function logShout(string3) {console.log('${logshout.toUpperCase()}')
+}
