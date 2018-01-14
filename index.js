02bc920ad909aa5287755d017fe9df0707d7dc73
Cap10Kirk <qrkirkland@gmail.com>
2018-01-14 04:34:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function sayHiToGrandma(string) {
+  if(string==='hello')
+  return "I can't hear you!"
+}
+function sayHiToGrandma(string) {
+  if(string==='CAN YOU HEAR ME')
+  return "YES INDEED!"
+}
