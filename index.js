ae612e0dd51e92e316e72376bfbb07c9bd5a1e97
morsele <morsl188@newschool.edu>
2018-01-14 04:21:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  return string.toLowerCase('I can\'t hear you!')
+  return string.toLowerCase()
 }
