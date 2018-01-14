cf0b8ca9649bedb2b2e4425235f494dd1c4abd47
morsele <morsl188@newschool.edu>
2018-01-14 04:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ function whisper(string){
   return string.toLowerCase()
 }
 function logShout(string){
-console.log(string.toUpperCase());
+console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
 }
