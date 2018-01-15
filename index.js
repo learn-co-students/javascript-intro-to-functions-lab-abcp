fc8f8d2142d556adc17416dae1b8b0d260fcc968
Shannon Prescott <shan.prescott@outlook.com>
2018-01-15 18:26:51 +0000

    Done.

--- index.js
+++ index.js
@@ -8,3 +8,25 @@ function whisper(string) {
   return string.toLowerCase()
 }
 whisper()
+
+function logShout(string) {
+  console.log(shout(string))
+}
+
+function logWhisper(string) {
+  console.log(whisper(string))
+}
+
+function sayHiToGrandma(string) {
+  if (string === whisper(string)) {
+    return "I can't hear you!"
+  }
+  
+  else if (string === shout(string)) {
+    return "YES INDEED!"
+  }
+  
+  else if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
