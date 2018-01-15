d9a4cf15b01a1c6864041de64b698083fbd28bb0
Tara Ellicott <taraellicott@gmail.com>
2018-01-15 17:04:20 +0000

    Done.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
     return "I love you, too.";
   }
   else if (string === string.toLowerCase()) {
-  return "I can\'t hear you!" ;
+  return "I can\'t hear you!";
 }
 else if (string === string.toUpperCase()){
   return "YES INDEED!";
