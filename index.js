03e601fe97d07ddb243ebadc3f6f2bf01fec0b47
Tara Ellicott <taraellicott@gmail.com>
2018-01-15 17:03:35 +0000

    Done.

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
-    return "I love you, too."
+    return "I love you, too.";
   }
   else if (string === string.toLowerCase()) {
-  return "I can\'t hear you!" 
+  return "I can\'t hear you!" ;
 }
 else if (string === string.toUpperCase()){
-  return "YES INDEED!"
+  return "YES INDEED!";
 } else {
   return string.toUpperCase();
 }
