579a97ea22f622022dad4e612438743eb1e1ddc3
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-17 20:23:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function whisper (string){
+return string.toLowerCase()
+}
+function shout (string){
+return string.toUpperCase()
+}
+function sayHiToGrandma (string) {
+  if (string ===string.toLowerCase()){
+  return ('I can\'t hear you!')
+  
+}   else if (string === string.toUpperCase()){
+  return ("YES INDEED!")}
+  else {
+    return ("I love you, too.")
+  }}
+
+function logShout(string) {
+  var string = string.toUpperCase();
+  console.log (string)
+}
+function logWhisper(string) {
+  var string = string.toLowerCase();
+  console.log (string)
+}
