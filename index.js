8cd0b3701f1c6cb60bf5bf9715142ee3894c5f04
Novera Lim <lim.novera88@gmail.com>
2018-01-22 18:29:00 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+var sayHiToGrandma = hello
+  console.log('sayHiToGrandma')
+  expect(sayHiToGrandma('hello!')).toEqual('I cant hear you!')
+}
+
+var sayHiToGrandma = HELLO
+  console.log('sayHiToGrandma')
+  expect(sayHiToGrandma('HELLO!')).toEqual('YES INDEED!')
+}
+
+
+  
