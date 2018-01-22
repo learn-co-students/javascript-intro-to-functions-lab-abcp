fda3043379a85dbbbe4fee988226685d88de45af
Sabeena N Singhani <sabeena@nyu.edu>
2018-01-22 14:51:31 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,5 @@
+function shout(string) { return string.toUpperCase () }
+function whisper(string) { return string.toLowerCase () }
+function sayHiToGrandma(string) { var lowercase = 'hello!' if (lowercase.toLowerCase() === lowercase) {return "I can't hear you!"} } 
+console.log(Shout) (`The header is: ${Shout}`)
+console.log(Whisper) 
