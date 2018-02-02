2f9aa3c6512b2790fb7a97f0631dd61b29a1191f
Soumya Anantha Padmanabha <soumya_anantha@yahoo.com>
2018-02-02 22:43:32 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+/*function shout(string) {
+  var uppercase = 'HELLO';
+  uppercase.toUpperCase() === uppercase;
+  console.log(string.toUpperCase());
+}*/
+/*describe('shout(string)', () => {
+  it('receives one argument and returns it in all caps', () => {
+    expect(shout('hello')).toEqual('HELLO')
+  })
+})*/
+function sayHiToGrandma(){
+  //var uppercase = 'HELLO';
+  uppercase.toUpperCase() === uppercase;
+  console.log(string.toUpperCase());
+}
