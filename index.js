309b0f2334b27c8c4b3007f9fc8d0fe45c15da4b
Aziz Kosber <a.kosber@gmail.com>
2018-03-05 17:20:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
-function shout(string) {
+function shout (string) {
+
   return string.toUpperCase()
+
 }
-function whisper(string) {
+
+function whisper (string) {
+
   return string.toLowerCase()
+
 }
 
-function logShout(string) {
-  let HELLO = string.toUpperCase
-  console.log('HELLO')
+function logShout (string) {
+
+  let HELLO = string.toUpperCase()
+
+  console.log(HELLO)
 }
 
-function logWhisper(string) {
-  let hello = string.toLowerCase
-  console.log ('hello')
+function logWhisper (string) {
+
+  let hello = string.toLowerCase()
+
+  console.log(hello)
+
 }
+
 function sayHiToGrandma(string) {
-  if(string.toLowerCase() === string) {
-      return "I can't hear you!"
-  } else if(string.toUpperCase() === string) {
-      return "YES INDEED!"
-  } else if(string === "I love you, Grandma.") {
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+
+  if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  }
+
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
